b5280750b7f450c6c3ff1f6f36701e552f02a13d
Deivison Santos <deivisonddj3@gmail.com>
2026-02-21 21:10:57 -0300

    fetch data using openCnpj

--- src/services/fetch_handler.py
+++ src/services/fetch_handler.py
@@ -3,7 +3,7 @@ import requests
 class FetchHandler:
 
     @staticmethod
-    def fetch_cnpj_data(cnpj:str):
+    def fetch_cnpj_data_on_cnpja(cnpj:str):
         url = f"https://open.cnpja.com/office/{cnpj}"
         response = requests.get(url)
 
@@ -16,5 +16,21 @@ class FetchHandler:
             except Exception:
                 message = response.text  # fallback caso não seja JSON
 
-            print(f'ERROR - Failed fetch by CNPJ [CNPJ: {cnpj}, statusCode: {response.status_code}, message: {message}].')
+            print(f'ERROR - Failed fetch by CNPJ with CnpJá [CNPJ: {cnpj}, statusCode: {response.status_code}, message: {message}].')
+            return None
+    
+    def fetch_cnpj_data_on_open_cnpj(cnpj:str):
+        url = f"https://api.opencnpj.org/{cnpj}"
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            return response.json()
+        else:
+            try:
+                error_data = response.json()
+                message = error_data.get("message")
+            except Exception:
+                message = response.text  # fallback caso não seja JSON
+
+            print(f'ERROR - Failed fetch by CNPJ with OpenCNPJ [CNPJ: {cnpj}, statusCode: {response.status_code}, message: {message}].')
             return None
